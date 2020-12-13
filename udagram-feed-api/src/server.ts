925c8b7a70bc47ddb2d6564e05e9c79540aca962
beber89 <mahmoudbaibars@gmail.com>
2020-12-13 18:07:44 +0200

    fixed fatality in dockerfiles of backends

--- udagram-feed-api/src/server.ts
+++ udagram-feed-api/src/server.ts
@@ -25,7 +25,7 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
+    origin: process.env.URL,
   }));
 
   app.use(cors({
@@ -35,17 +35,17 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
+    origin: process.env.URL,
   }));
   
   // Added this to help encounter cors issue while testing locally
   app.options("*", function(req, res, next) {     
-    res.header("Access-Control-Allow-Origin", config.url);     
+    res.header("Access-Control-Allow-Origin", process.env.URL);     
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization' );    
     next();  
   });
   app.use(function(req, res, next) {     
-    res.header("Access-Control-Allow-Origin", config.url);     
+    res.header("Access-Control-Allow-Origin", process.env.URL);     
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization' );    
     next();  
   });
